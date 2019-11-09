d3aba70a849ff1fe719897688b091c193f5cb71a
clementoys <clementoys@gmail.com>
2019-11-09 18:57:59 +0800

    customerservice php and js

--- js/customerservice.js
+++ js/customerservice.js
@@ -1,37 +1,24 @@
 function validateform() {
-    var ciname = document.forms["CustomerInquiries"]["CIname"].value;
-    var ciemail = document.forms["CustomerInquiries"]["CIemail"].value;
-    var cinumber = document.forms["CustomerInquiries"]["CInumber"].value;
-    var ciquestion = document.forms["CustomerInquiries"]["CIquestion"].value;
+  var ciname = document.forms["CustomerInquiries"]["CIname"].value;
+  var ciemail = document.forms["CustomerInquiries"]["CIemail"].value;
+  var cinumber = document.forms["CustomerInquiries"]["CInumber"].value;
+  var ciquestion = document.forms["CustomerInquiries"]["CIquestion"].value;
 
-    if (ciname == "") {
-      alert("Username must be filled out");
+  if (ciname === "" || ciemail === "" || cinumber === "" || ciquestion === "") {
+      alert("All fields must be filled out");
       return false;
-    }
-
-    if (ciname != /[^a-zA-Z\-\/]/) {
-        alert("No special characters allowed!");
-      return false;
-    }
-
-    if (ciemail == "") {
-        alert("Username must be filled out");
-        return false;
-      }
-
-      if (cinumber == "") {
-        alert("Username must be filled out");
-        return false;
-      }
-  
-      if (cinumber != /[^0-9\-\/]/) {
-          alert("No special characters and alphabets allowed!");
-        return false;
-      }
-
-      if (ciquestion == "") {
-        alert("Username must be filled out");
-        return false;
-      }
-
   }
+
+  reName = /^([a-zA-Z]+[,.]?[ ]?|[a-zA-Z]+['-]?)+$/;
+  if (reName.test(ciname) === false) {
+      alert("Name is in invalid format.");
+      return false;
+  }
+
+  reNumber = /[0-9]{8,8}/;
+  if (reNumber.test(cinumber) === false) {
+      alert("Phone number is in invalid format.");
+      return false;
+  }
+}
+$("#submitbutton").on("click", validateform);
