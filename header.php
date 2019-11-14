64a0f29b7372fa6de04de51eab27ca5496b67c94
DanielLekLXY <56250211+DanielLekLXY@users.noreply.github.com>
2019-11-14 09:42:27 +0800

    trying  to put name in navbar

--- header.php
+++ header.php
@@ -57,9 +57,9 @@ if (!isset($_SESSION)) {
                         <!--<li><a href="products.php#hot">Top selling</a></li>
                         <li><a href="products.php#newarr">Newest</a></li>
                         <li><a href="products.php">All</a></li>-->
-                        <li><a href="product.php#hot">Top selling</a></li>
-                        <li><a href="product.php#newarr">Newest</a></li>
-                        <li><a href="product.php">All</a></li>
+                        <li><a href="product_database.php#hot">Top selling</a></li>
+                        <li><a href="product_database.php#newarr">Newest</a></li>
+                        <li><a href="productdetails.php">All</a></li>
                     </ul>
                 </li>
                 <li class="dropdown">
@@ -88,7 +88,8 @@ if (!isset($_SESSION)) {
 
                 <?php
                 if (isset($_SESSION['firstName'])) {
-                    echo'<h4 class="navbar-right"> ' . $_SESSION['firstName'] . '</h4>';
+                    $firstName = $_SESSION['firstName']
+                    echo'<h4 class="navbar-right"> ' . $firstName . '</h4>';
                 }
                 ?>
         </section>
