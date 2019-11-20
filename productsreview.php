0799be8d230dbe73f7d575cdd97768160a9d4ef1
HeyAaronHere <aaron.chua@hotmail.com>
2019-11-20 22:17:05 +0800

    AC

--- productsreview.php
+++ productsreview.php
@@ -11,92 +11,93 @@ $result = "";
 
 
 include "connection.inc.php";
-?>
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <title>Reviews - ONTime</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="css/bootstrap.min.css" rel="stylesheet">
-        <link rel="stylesheet" href="css/reviews.css" type="text/css" />
-        <link rel="stylesheet" href="css/headerFooter.css" type="text/css" />
-        <script src="js/productreview.js"></script>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js">
-        </script>
-        <script src="js/bootstrap.min.js">
-        </script>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    </head>
-    <body>
-        <?php
-        include "header.inc.php";
-        ?>
-        <main class="review container-fluid">
-            <?php if (isset($_SESSION['msg'])): ?>
-                <div class="msg" id="h2">
+
+$query = "SELECT MAX(review_id) as MAX_REVIEW FROM review";
+$sql = mysqli_query($conn, $query);
+if (!$sql) {
+    $errorMsg .= "<p>Database error: " . $conn->error . "</p>";
+    $success = false;
+} else if (mysqli_num_rows($sql) > 0) {
+    $row = mysqli_fetch_assoc($sql);
+    $reviewID = $row['MAX_REVIEW'];
+}
+
+$IDquery = "SELECT * FROM review WHERE review_id ='$reviewID'";
+$IDsql = mysqli_query($conn, $IDquery);
+if (!$IDsql) {
+    $errorMsg .= "<p>Database error: " . $conn->error . "</p>";
+    $success = false;
+} else if (mysqli_num_rows($IDsql) > 0) {
+
+    $reviewDetails = mysqli_fetch_assoc($IDsql);
+    ?>
+
+    <!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <title>Reviews - ONTime</title>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link href="css/bootstrap.min.css" rel="stylesheet">
+            <link rel="stylesheet" href="css/reviews.css" type="text/css" />
+            <link rel="stylesheet" href="css/headerFooter.css" type="text/css" />
+            <script src="js/productreview.js"></script>
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js">
+            </script>
+            <script src="js/bootstrap.min.js">
+            </script>
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        </head>
+        <body>
+            <?php
+            include "header.inc.php";
+            ?>
+            <main class="review container-fluid">
+                <?php if (isset($_SESSION['msg'])): ?>
+                    <div class="msg" id="h2">
+                        <?php
+                        echo $_SESSION['msg'];
+                        unset($_SESSION['msg']);
+                        ?>
+                    </div>
+                <?php endif ?>
+
+
+                <h1>Reviews</h1>
+                <p class="text-center">We value every comments you provide!</p>
+                <div class="container" id="productreview">
+                    <!--Responsive Form CSS - https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_responsive_form -->
+                    <!-- w3schools js validation, https://www.w3schools.com/js/js_validation.asp -->
+                    <form id="productreviews" action="process_review.php" method="post">
+                        <div class="col-sm-12 form-group">
+                            <label for="review">
+                                Reviews:</label>
+                            <textarea class="form-control" id="reviewinput" name="review" placeholder="Write your reviews.." maxlength="6000" rows="7" required></textarea>
+                        </div>
+                        <div class="col-sm-6 form-group">
+                            <label for="name">
+                                Your Name:</label>
+                            <input type="text" class="form-control" id="name" name="name" pattern="^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$" required>
+                        </div>
+                        <div class="col-sm-6 form-group">
+                            <label for="email">
+                                Email:</label>
+                            <input type="email" class="form-control" id="email" name="email"  pattern="\S+@\S+\.\S+" required>
+                        </div>
+
+                        <input type="submit" id="btnSubmit" name ="submitbutton" value="Submit">
+                    </form>
                     <?php
-                    echo $_SESSION['msg'];
-                    unset($_SESSION['msg']);
                     ?>
                 </div>
-            <?php endif ?>
 
-
-            <h1>Reviews</h1>
-            <p class="text-center">We value every comments you provide!</p>
-            <div class="container" id="productreview">
                 <!--Responsive Form CSS - https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_responsive_form -->
-                <!-- w3schools js validation, https://www.w3schools.com/js/js_validation.asp -->
-                <form id="productreviews" action="process_review.php" method="post">
-                    <div class="col-sm-12 form-group">
-                        <label for="review">
-                            Reviews:</label>
-                        <textarea class="form-control" id="reviewinput" name="review" placeholder="Write your reviews.." maxlength="6000" rows="7" required></textarea>
-                    </div>
-                    <div class="col-sm-6 form-group">
-                        <label for="name">
-                            Your Name:</label>
-                        <input type="text" class="form-control" id="name" name="name" pattern="^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$" required>
-                    </div>
-                    <div class="col-sm-6 form-group">
-                        <label for="email">
-                            Email:</label>
-                        <input type="email" class="form-control" id="email" name="email"  pattern="\S+@\S+\.\S+" required>
-                    </div>
 
-                    <input type="submit" id="btnSubmit" name ="submitbutton" value="Submit">
-                </form>
-                <?php
-                ?>
-            </div>
+                <!-- review URL = https://www.youtube.com/watch?v=qZno3Yk8QKc&t=500s Author = Easy Tutorials -->
 
-            <!--Responsive Form CSS - https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_responsive_form -->
-
-            <!-- review URL = https://www.youtube.com/watch?v=qZno3Yk8QKc&t=500s Author = Easy Tutorials -->
-            <?php
-            $query = "SELECT MAX(review_id) as MAX_REVIEW FROM review";
-            $sql = mysqli_query($conn, $query);
-            if (!$sql) {
-                $errorMsg .= "<p>Database error: " . $conn->error . "</p>";
-                $success = false;
-            } else if (mysqli_num_rows($sql) > 0) {
-                $row = mysqli_fetch_assoc($sql);
-                $reviewID = $row['MAX_REVIEW'];
-            }
-
-            $IDquery = "SELECT * FROM review WHERE review_id ='$reviewID'";
-            $IDsql = mysqli_query($conn, $IDquery);
-            if (!$IDsql) {
-                $errorMsg .= "<p>Database error: " . $conn->error . "</p>";
-                $success = false;
-            } else if (mysqli_num_rows($IDsql) > 0) {
-
-                $reviewDetails = mysqli_fetch_assoc($IDsql);
-                ?>
                 <section class="container">
                     <div class="row review_box" >
                         <div class="col-sm-3">
