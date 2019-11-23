24481d1310007a42a7c04ba781587ae03b483456
clementoys <clementoys@gmail.com>
2019-11-23 16:55:47 +0800

    removal of .php links at header & footer

--- header.inc.php
+++ header.inc.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-custom navbar-fixed-top navbar-inverse">
     <div id="topnav-centered">
-        <a class="navbar-brand" href="index.php" title="Link to HomePage" ><img src="images/logo3.png" alt="Ontime logo"></a>
+        <a class="navbar-brand" href="index" title="Link to HomePage" ><img src="images/logo3.png" alt="Ontime logo"></a>
     </div>
     <section class="navbar-header" id="myNavBarLeft">
         <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
@@ -11,20 +11,20 @@
     </section>
     <section class="collapse navbar-collapse container-fluid" id="myNavbar">
         <ul class="nav navbar-nav">
-            <li><a href="product.php">Products</a></li>
-            <li><a href="customerservice.php">Customer Service</a></li>
-            <li><a href="productsreview.php">Reviews</a></li>
-            <li><a href="aboutus.php">About us</a></li>
+            <li><a href="product">Products</a></li>
+            <li><a href="customerservice">Customer Service</a></li>
+            <li><a href="productsreview">Reviews</a></li>
+            <li><a href="aboutus">About us</a></li>
         </ul>
         <ul class="nav navbar-nav navbar-right">
             <?php
             if (isset($_SESSION['firstName'])) {
-                echo'<li><a href="profile_edit.php"><span class="glyphicon glyphicon-user"></span>  ' . $_SESSION['firstName'] . '</a></li>';
-                echo'<li><a href="shoppingcart.php"><span class="glyphicon glyphicon-shopping-cart"></span> Cart</a></li>';
-                echo'<li><a href="logout.php"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>';
+                echo'<li><a href="profile_edit"><span class="glyphicon glyphicon-user"></span>  ' . $_SESSION['firstName'] . '</a></li>';
+                echo'<li><a href="shoppingcart"><span class="glyphicon glyphicon-shopping-cart"></span> Cart</a></li>';
+                echo'<li><a href="logout"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>';
             } else {
-                echo'<li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>';
-                echo '<li><a href="signup.php"><span class="glyphicon glyphicon-pencil"></span> Signup</a></li>';
+                echo'<li><a href="login"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>';
+                echo '<li><a href="signup"><span class="glyphicon glyphicon-pencil"></span> Signup</a></li>';
             }
             ?>
         </ul>
