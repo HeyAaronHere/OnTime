945095509bf8af73f046d5018b3b0e58cbf37980
clementoys <clementoys@gmail.com>
2019-11-23 16:32:04 +0800

    Revert "AC"
    This reverts commit 9823959ea4600bfd3293f5f0982aa437118ece59.

--- productdetails.php
+++ productdetails.php
@@ -44,43 +44,44 @@
             </head>
 
             <body>
-                <?php
-                include "header.inc.php";
+                <?php 
+                include "header.inc.php"; 
                 ?>
 
                 <!-- Bootstrap Carousel  W3school  URL = https://www.w3schools.com/bootstrap/bootstrap_carousel.asp
                     all images source from Cocomi.com URL = https://www.cocomi.com/
                     how to make a single product ecommerce URL https://www.youtube.com/watch?v=4zGBRBHsgEY Author- Easy Tutorials-->
-
-                <article class="container singleProduct">
-                    <section class=row>
-                        <div class = "col-md-5">
-                            <img src="<?php echo $productDetails['product_img'] ?>" alt="<?php echo $productDetails['product_alt'] ?> ">
-                        </div>
-                        <div class = "col-md-7">
-                            <p class = "new-arrival text-center">NEW</p>
-                            <h2><?php echo $productDetails['product_name'] ?></h2>
-                            <p><b>Product Code:</b> <?php echo $productDetails['product_code'] ?></p>
-                            <p id="price">$<?php echo $productDetails['product_price'] ?></p>
-                            <p><b>Availability:</b> <?php echo $productDetails['product_stock'] ?> </p>
-                            <p><b>Condition:</b> New </p>
-                            <p><b>Brand:</b> <?php echo $productDetails['product_brand'] ?></p>
-                            <label for="productinput" >Quantity </label>
-                            <form id="productdetails" action="process_shoppingcartitem.php" method="post">
-                                <input type="number" id ="productinput" min="1" name="productAmount" value="1">
-                                <input type="hidden"  name="productPrice" value="<?php echo $productDetails['product_price'] ?>">
-                                <input type="hidden" name="productID" value="<?php echo $productDetails['product_id'] ?>">
-                                <button type="submit" id="btnSubmit" value="Submit">Add to Cart</button>
-                            </form>
-                        </div>
+                <article class="singleProduct">
+                    <section class="container">
+                        <section class=row>
+                            <div class = "col-md-5">
+                                <img src="<?php echo $productDetails['product_img'] ?>" alt="<?php echo $productDetails['product_alt']?> ">
+                            </div>
+                            <div class = "col-md-7">
+                                <p class = "new-arrival text-center">NEW</p>
+                                <h2><?php echo $productDetails['product_name'] ?></h2>
+                                <p><b>Product Code:</b> <?php echo $productDetails['product_code'] ?></p>
+                                <p id="price">$<?php echo $productDetails['product_price'] ?></p>
+                                <p><b>Availability:</b> <?php echo $productDetails['product_stock'] ?> </p>
+                                <p><b>Condition:</b> New </p>
+                                <p><b>Brand:</b> <?php echo $productDetails['product_brand'] ?></p>
+                                <label for="productdetails">Quantity </label>
+                                <form id="productdetails" action="process_shoppingcartitem.php" method="post">
+                                    <input type="number" id ="productinput" min="1" name="productAmount" value="1">
+                                    <input type="hidden"  name="productPrice" value="<?php echo $productDetails['product_price'] ?>">
+                                    <input type="hidden" name="productID" value="<?php echo $productDetails['product_id'] ?>">
+                                    <button type="submit" id="btnSubmit" value="Submit">Add to Cart</button>
+                                </form>
+                            </div>
+                        </section>
                     </section>
-
-
-                    <section class="container productDescription">
-                        <h3>Product Description</h3>
-                        <p><?php echo $productDetails['product_desc'] ?></p>
-                        <p><?php echo $productDetails['product_desc'] ?></p>
-                        <hr>
+                    <section class="productDescription">
+                        <section class="container">
+                            <h3>Product Description</h3>
+                            <p><?php echo $productDetails['product_desc'] ?></p>
+                            <p><?php echo $productDetails['product_desc'] ?></p>
+                            <hr>
+                        </section>
                     </section>
                     <?php
                 }
