a7aa807778165e0883e1e72025eeb10473c9d20b
clementoys <clementoys@gmail.com>
2019-11-23 18:38:27 +0800

    fix daniel's and aaron's

--- productsreview.php
+++ productsreview.php
@@ -40,17 +40,16 @@ if (!$IDsql) {
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <link href="css/bootstrap.min.css" rel="stylesheet">
-            <link rel="stylesheet" href="css/reviews.css" type="text/css" />
-            <link rel="stylesheet" href="css/headerFooter.css" type="text/css" />
-            <script src="js/productreview.js"></script>
-            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js">
-            </script>
-            <script src="js/bootstrap.min.js">
-            </script>
+            <link rel="stylesheet" href="css/reviews.css" type="text/css">
+            <link rel="stylesheet" href="css/headerFooter.css" type="text/css">
             <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+            
+            <script src="js/productreview.js"></script>
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+            <script src="js/bootstrap.min.js"></script>
             <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
             <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
         </head>
         <body>
             <?php
