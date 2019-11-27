17d4a440f9d5bbae37cfb720f1d3a6ca352025e1
clementoys <clementoys@gmail.com>
2019-11-27 15:50:50 +0800

    form js fix

--- js/customerservice.js
+++ js/customerservice.js
@@ -1,24 +1,29 @@
-function validateform() {
-  var ciname = document.forms["CustomerInquiries"]["CIname"].value;
-  var ciemail = document.forms["CustomerInquiries"]["CIemail"].value;
-  var cinumber = document.forms["CustomerInquiries"]["CInumber"].value;
-  var ciquestion = document.forms["CustomerInquiries"]["CIquestion"].value;
+window.onload = function () {
+    document.getElementById("CI-submit").addEventListener("click", validateCIform);
+    function validateCIform() {
+        var ciname = document.forms["CustomerInquiries"]["CIname"].value;
+        var ciemail = document.forms["CustomerInquiries"]["CIemail"].value;
+        var cinumber = document.forms["CustomerInquiries"]["CInumber"].value;
+        var ciquestion = document.forms["CustomerInquiries"]["CIquestion"].value;
 
-  if (ciname === "" || ciemail === "" || cinumber === "" || ciquestion === "") {
-      alert("All fields must be filled out");
-      return false;
-  }
+        if (ciname === "" || ciemail === "" || cinumber === "" || ciquestion === "") {
+            alert("All fields must be filled out");
+            event.preventDefault();
+            return false;
+        }
 
-  reName = /^([a-zA-Z]+[,.]?[ ]?|[a-zA-Z]+['-]?)+$/;
-  if (reName.test(ciname) === false) {
-      alert("Name is in invalid format.");
-      return false;
-  }
+        reName = /^([a-zA-Z]+[,.]?[ ]?|[a-zA-Z]+['-]?)+$/;
+        if (reName.test(ciname) === false) {
+            alert("Name is in invalid format.");
+            event.preventDefault();
+            return false;
+        }
 
-  reNumber = /[0-9]{8,8}/;
-  if (reNumber.test(cinumber) === false) {
-      alert("Phone number is in invalid format.");
-      return false;
-  }
-}
-$("#CI-submit").on("click", validateform);
+        reNumber = /[0-9]{8,8}/;
+        if (reNumber.test(cinumber) === false) {
+            alert("Phone number is in invalid format.");
+            event.preventDefault();
+            return false;
+        }
+    }
+};
--- js/login.js
+++ js/login.js
@@ -1,26 +1,32 @@
-function validateloginform() {
-  var email = document.forms["LoginForm"]["email"].value;
-  var pwd = document.forms["LoginForm"]["pwd"].value;
+window.onload = function () {
+    document.getElementById("login-submit").addEventListener("click", validateloginform);
 
-  if (email === "" || pwd === "") {
-      alert("Forms can't be blank");
-      return false;
-  } else if (pwd.length < 8) {
-      alert("Password must be at least 8 characters long.");
-      return false;
-  }
-  
-  reEmail = /[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,63}$/;
-  if (reEmail.test(email) === false){
-      alert("Email is in invalid format.");
-      return false;
-  }
-  
-  rePwd = /(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/;
-  if (rePwd.test(pwd) === false) {
-      alert("Password is in invalid format");
-      return false;
-  }
-}
+    function validateloginform(event) {
+        var email = document.forms["LoginForm"]["email"].value;
+        var pwd = document.forms["LoginForm"]["pwd"].value;
 
-$("#login-submit").on("click", validateloginform);
+        if (email === "" || pwd === "") {
+            alert("Forms can't be blank");
+            event.preventDefault();
+            return false;
+        } else if (pwd.length < 8) {
+            alert("Password must be at least 8 characters long.");
+            event.preventDefault();
+            return false;
+        }
+
+        reEmail = /[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,63}$/;
+        if (reEmail.test(email) === false) {
+            alert("Email is in invalid format.");
+            event.preventDefault();
+            return false;
+        }
+
+        rePwd = /(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/;
+        if (rePwd.test(pwd) === false) {
+            alert("Password is in invalid format");
+            event.preventDefault();
+            return false;
+        }
+    }
+};
